aa6f9bdcec76299898c49b85494366a3d7ed9b94
AWGGG <51860413+AWGGG@users.noreply.github.com>
2023-11-13 23:56:55 +0800

    CPP Primer BookStore App

---
+++ ChapterOne/Section6/BookstoreApp/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by yi.chen03 on 2023/11/13.
+//
+#include <iostream>
+#include "../../../Utils/Sales_item.h"
+
+int main()
+{
+	// 将每个ISBN的数据合并起来, 存入total变量
+	// 使用另一个名为trans的变量保存读取每一条销售记录
+
+	Sales_item total;
+	if(std::cin >> total)
+	{
+		Sales_item trans;
+		while(std::cin >> trans)
+		{
+			if(total.isbn() == trans.isbn())
+				total += trans;
+			else
+			{
+				std::cout << total << "\n";
+				total = trans;
+			}
+		}
+		std::cout << total << "\n";
+	}
+	else
+	{
+		std::cerr << "No data?\n";
+		return -1;
+	}
+	return 0;
+}
